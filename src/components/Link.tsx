34b9a6291d0ae2b7736ef133b26013b39b7062b6
C-RodrigoAlmeida <c.almeidarodrigo@gmail.com>
2025-02-19 03:36:40 -0300

    Link component added to the project

---
+++ src/components/Link.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+import classNames from "classnames";
+import { useNavigate, useLocation } from "react-router-dom";
+
+
+interface LinkProps {
+    to: string;
+    children: ReactNode;
+    className: string;
+    activeClassName: boolean;
+}
+
+function Link({ to, children, className, activeClassName }: LinkProps) {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+  const classes = classNames(
+    'text-blue-500', 
+    className,
+    location.pathname === to && activeClassName
+  );
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
+     
+    e.preventDefault();
+    navigate(to);
+  };
+
+  return <a className={classes} href={to} onClick={handleClick}>{children}</a>
+}
+
+export default Link;
+
