7b57c4e8549dedcb4445dca9878bec00bcf61542
C-RodrigoAlmeida <c.almeidarodrigo@gmail.com>
2025-02-25 17:26:40 -0300

    panel component removal from root.tsx

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Panel from "../components/Panel";
 export default function Root() {
 
     return(
         <div className="bg-neutral-950 h-screen w-screen bg-cover bg-center bg-no-repeat text-[#98abfc]">
             <div className="container mx-auto">
                 <Navbar />
-                <Panel className="min-h-128 max-h-210 rounded-4xl shadow-lg p-20">
-                    <Outlet />
-                </Panel>
+                <Outlet />
             </div>
         </div>
     )
